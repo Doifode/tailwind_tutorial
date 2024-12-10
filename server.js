c2471a9cdb64e9c08f4fdf05bf0ab7874c55925e
doifode <yashdada9001@gmail.com>
2024-12-10 16:17:50 +0530

    fix:changes to fix payload storage

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // For parsing JSON
 app.use(express.urlencoded({ extended: true })); // For parsing form data
+app.use(express.json({ limit: '2mb' })); // Set limit to 2MB for JSON payloads
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 
 // Set up MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -78,7 +80,7 @@ app.use(express.static(path.join(__dirname, '/tree-qr-generator/dist')));
 
 // Catch-all route to serve the React app (for any route not starting with "/api")
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'tree-qr-generator', 'dist','index.html'));
+    res.sendFile(path.join(__dirname, 'tree-qr-generator', 'dist', 'index.html'));
 });
 
 // Start the server
